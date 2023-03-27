08607b9a5e4a7ba4ae3fbe496142044aeea5ecc1
Alisa Porras <aljuporras@gmail.com>
2023-03-27 16:21:14 -0400

    done with main2

---
+++ src/main/java/arrayPractice/Main2.java
@@ -0,0 +1,18 @@
+package arrayPractice;
+
+public class Main2
+{
+//    Write a program that returns the middle element in an array. Give the following
+//    values to the integer array: {13, 5, 7, 68, 2} and produce the following output: 7
+
+    public static void main(String[] args)
+    {
+       int [] middleNumberArray = {13, 5, 7, 68, 2};
+
+       int holdMiddleNumber = middleNumberArray[2];
+
+        System.out.println("middle number in array: " + holdMiddleNumber);
+    }
+
+
+}
