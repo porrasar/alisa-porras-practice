59fbe27329ef8ed5a521fc25e2659942699f74ae
Alisa Porras <aljuporras@gmail.com>
2023-03-27 17:34:45 -0400

    done with main2

--- src/main/java/arrayPractice/Main2.java
+++ src/main/java/arrayPractice/Main2.java
@@ -3,7 +3,7 @@ package arrayPractice;
 public class Main2
 {
 //    Write a program that returns the middle element in an array. Give the following
-//    values to the integer array: {13, 5, 7, 68, 2} and produce the following output: 7
+//     values to the integer array: {13, 5, 7, 68, 2} and produce the following output: 7
 
     public static void main(String[] args)
     {
